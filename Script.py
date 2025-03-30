940339ee7690d90ad1d6d2f87669ec04302ad5d8
Kamalesh9787 <kamaleshking957@gmail.com>
2025-03-30 17:02:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -257,7 +257,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>{fil_ename}\n\n
+    CAPTION = """<b>{file_name}\n\n
 ┎┈┈┈┈┈┈┈୨♡୧┈┈┈┈┈┈┈┒
      Join : 💕 @Tamil_Mini
 ┖┈┈┈┈┈┈┈୨♡୧┈┈┈┈┈┈┈┚</b>""" 
