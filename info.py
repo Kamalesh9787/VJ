c9d6ebc873591b6a556adcf3176997a20dd527a9
Kamalesh9787 <kamaleshking957@gmail.com>
2025-04-18 08:47:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -77,7 +77,7 @@ S_DB_URI = environ.get('S_DB_URI', "")   # This Db is for File Data Store When F
 
 
 # Premium And Referal Settings
-PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', True)) # Set Ture Or False
+PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', False)) # Set Ture Or False
 
 # If PREMIUM_AND_REFERAL_MODE is True Then Fill Below Variable, If Flase Then No Need To Fill.
 REFERAL_COUNT = int(environ.get('REFERAL_COUNT', '20')) # number of referal count
@@ -131,7 +131,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'techvjlink.site')
 SHORTLINK_API = environ.get('SHORTLINK_API', '4f39e46d4e086e60edddb2059c6a57d632ee9db3')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/tamil_movie_linkz') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
